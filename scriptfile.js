a8e0f772ae974fedbc924487a442893f27a5571f
Developer-Fern <72754915+Developer-Fern@users.noreply.github.com>
2023-03-23 18:34:14 -0400

    Add files via upload

---
+++ scriptfile.js
@@ -0,0 +1,21 @@
+/*
+1. Create HTML file and provide necessarily code to structure it
+2. Create a new JS file and link it to the html
+3. Assign each digit to it's respective combonation number using arthematic operators
+4. set up multple alter lines to showcase dialog boxes to user
+*/
+
+//This line will pop up as a dialog screen as the first thing the user sees
+
+let digit1 = (5 *2)
+let digit2 = (100 - 60)
+let digit3 = (396 % 357)
+alert('You have received this message because you have been chosen to open an important vault. To check the secret combination press:  OK');
+
+alert('Here is the combination:')
+alert(digit1)
+alert(digit2)
+alert(digit3)
+
+
+console.log('Here is the combination:' + " " + digit1, digit2, digit3)
